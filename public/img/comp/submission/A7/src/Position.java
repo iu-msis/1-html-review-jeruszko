dcfbc625d9840ceda2c5f32b785a3b27b023c9d1
jeruszko <jeruszko@iu.edu>
2021-09-03 23:55:05 -0400

    quick change

--- public/img/comp/submission/A7/src/Position.java
+++
@@ -1,5 +0,0 @@
-public class Position extends Pair<Integer,Integer> {
-    Position (int h, int w) {
-        super(h,w);
-    }
-}
--- public/img/comp/submission/README.md
+++
@@ -1,2 +0,0 @@
-# distribution
-All the starter code for assignments.
