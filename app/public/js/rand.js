a125b9830527a1c91c326021dbc42a03bd4d91a3
jeruszko <jeruszko@iu.edu>
2021-09-22 23:57:35 -0400

    done - 4b

--- app/public/js/rand.js
+++ app/public/js/rand.js
@@ -11,15 +11,23 @@ const app = ({
             return dayjs(this.results.dob.date).format('DD/MM/YYYY')
          }
     },
-    created(){
-    console.log("a");
-    fetch('https://randomuser.me/api/')
-        .then( response => response.json())
-        .then((parsedJson) => {
-            this.results = parsedJson.results[0]
+
+    methods: {
+        fetchUserData(){
+            console.log("a");
+            fetch('https://randomuser.me/api/')
+            .then( response => response.json())
+            .then((parsedJson) => {
+            this.results = parsedJson.results[0];
         })
-        .catch ( err => 
-            console.error(err))
+        .catch ( err => {
+            console.error(err);
+        })
+        }
+
+    },
+    created() {
+        this.fetchUserData();
     }
 })
 Vue.createApp(app).mount('#app')
