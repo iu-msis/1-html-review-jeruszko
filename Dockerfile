45ca0197b4375c8c285d1092457c1ea33e123e95
jeruszko <jeruszko@iu.edu>
2021-10-06 02:37:04 -0400

    push for in-class (MISSING docker connection)

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,14 @@ FROM php:7.4-apache
 
 LABEL maintainer="Jack Ruszkowski"
 
-#Copy our public folder to the working directory 
+#set our public folder to the working directory 
+WORKDIR /srv/app
 
+#Copy our app folder to the image
 COPY app /srv/app
 
+# PHP configuration
+COPY docker/php/php.ini /usr/local/etc/php/php.ini
+
+# Apache configuration
 COPY docker/apache/vhost.conf /etc/apache2/sites-available/000-default.conf
---
+++ app/public/phpinfo.php
@@ -0,0 +1,38 @@
+<?php
+
+phpinfo(); //brings up api
+
+/* Not sure about after this ***
+//every variable starts with $
+$num =2;
+$foo = "test";
+$bar = "or not ".$num." be";
+
+echo $foo .' ' . $bar . "\n";
+
+echo $num * $num * $num;
+
+$arr = [
+    "first" => "Tom",
+    "Second" => "Bip",
+    "third" => "balch"
+]
+
+if (true) {
+    echo "\nTRUE\n";
+}
+
+while(true){
+    break;
+}
+
+// echo "<ul>";
+// foreach($arr as $key=>$val){
+//     echo "<li>".$key ." is ".$val
+// }
+
+
+//Bad paractice to have php in html. Get rid of closing tag to reduce bug?
+//MUST USE DOCKER GOING FORWARD!!!!
+# this
+/*this*/
