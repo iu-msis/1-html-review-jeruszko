976cbc01a815399036763eb82b76204250410a57
jeruszko <jeruszko@iu.edu>
2021-10-09 20:23:42 -0400

    almost there -- just not showing up

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -9,8 +9,23 @@ const BookApp = {
         prettyDollar(n) {
             const d = new Intl.NumberFormat("en-US").format(n);
             return "$ " + d;
+        },
+        fetchBookData() {
+            fetch('/api/books/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.books = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
         }
-    }
+        
+    },
+    created() {
+            this.fetchBookData();
+        }
   
   }
   
