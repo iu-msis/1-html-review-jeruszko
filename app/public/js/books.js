4d0fc33d81db6514c3eab7106a822e5e6b79ddc5
jeruszko <jeruszko@iu.edu>
2021-10-20 19:44:42 -0400

    HMW 6 complete - 7;p

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,7 +1,9 @@
 const BookApp = {
     data() {
       return {
-        books: []
+        books: [],
+        selectedBook: null, //maybe
+        bookForm: {}
       }
     },
     computed: {},
@@ -20,7 +22,31 @@ const BookApp = {
             .catch( (err) => {
                 console.error(err);
             })
-        }
+        },
+        postNewBook(evt) {
+            //this.bookForm.id = this.selectedBook.id; //not sure about this
+            console.log("Posted!", this.bookForm); //test to make sure form works  //use cmd+shift+r to make sure
+            alert("Post");
+
+            fetch('api/books/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json; //idk
+                
+                // reset the form
+                this.bookForm = {};
+              });
+
+            
+            }
         
     },
     created() {
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -5,6 +5,7 @@ const SomeApp = {
         selectedStudent: null,
         offers: [], 
         //books: []
+        bookForm: {}
       }
     },
     computed: {},
