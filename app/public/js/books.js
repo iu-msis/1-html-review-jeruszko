05007b7f87ab3933789531543d4b930326f40b30
jeruszko <jeruszko@iu.edu>
2021-10-27 10:06:03 -0400

    update/delete

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -46,6 +46,64 @@ const BookApp = {
               });
 
             
+            },
+            postBook(evt) {
+              if (this.selectedBook === null) {
+                  this.postNewBook(evt);
+              } else {
+                  this.postEditBook(evt);
+              }
+            },
+            postEditBook(evt) {
+              // this.offerForm.studentId = this.selectedStudent.id;
+              this.bookForm.id = this.selectedBook.id;       
+              
+              console.log("Updating!", this.bookForm);
+      
+              fetch('api/books/update.php', {
+                  method:'POST',
+                  body: JSON.stringify(this.bookForm),
+                  headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                  }
+                })
+                .then( response => response.json() )
+                .then( json => {
+                  console.log("Returned from post:", json);
+                  // TODO: test a result was returned!
+                  this.books = json;
+                  
+                  this.resetBookForm();
+                });
+            },
+            postDeleteBook(o) {
+              if (!confirm("Are you sure you want to delete the offer from "+o.rname+"?")) {
+                  return;
+              }
+              
+              fetch('api/books/delete.php', {
+                  method:'POST',
+                  body: JSON.stringify(o),
+                  headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                  }
+                })
+                .then( response => response.json() )
+                .then( json => {
+                  console.log("Returned from post:", json);
+                  // TODO: test a result was returned!
+                  this.books = json;
+                  
+                  this.resetBookForm();
+                });
+            },
+            selectBook(o) {
+              this.selectedBook = o;
+              this.bookForm = Object.assign({}, this.selectedBook);
+            },
+            resetBookForm() {
+              this.selectedBook = null;
+              this.bookForm = {};
             }
         
     },
